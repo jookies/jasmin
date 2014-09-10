eb6eb654758d56321a5ee2528e4e52d29ca76357
Fourat ZOUARI <fourat@gmail.com>
2014-09-10 16:37:25 +0100

    travis-ci/packaging

--- jasmin/__init__.py
+++ jasmin/__init__.py
@@ -3,14 +3,16 @@
 
 """Jasmin SMS Gateway by Fourat ZOUARI <fourat@gmail.com>"""
 
-VERSION = '0.4'
-RELEASE = '%s.0-alpha' % VERSION
+MAJOR = 0
+MINOR = 4
+PATCH = 0
+META = 'beta'
 
 def get_version():
-    return VERSION
+    return '%s.%s' % (MAJOR, MINOR)
 
 def get_release():
-    return RELEASE
+    return '%s.%s.%s-%s' % (MAJOR, MINOR, PATCH, META)
 
 __version__ = get_version()
 __release__ = get_release()
