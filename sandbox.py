ad3a59550934fbb6281c617b94c84aaa4482f099
Fourat ZOUARI <fourat@gmail.com>
2015-02-05 20:40:08 +0100

    Debugging failing travis build

--- sandbox.py
+++ sandbox.py
@@ -1,5 +1,10 @@
 from pip.req import parse_requirements
 install_reqs = parse_requirements('install-requirements')
+
+print install_reqs
+for x in install_reqs:
+	print 'x:', x
+
 install_requires=[str(ir.req) for ir in install_reqs]
 
 print install_requires
