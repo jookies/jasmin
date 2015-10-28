9cb6559eb2247b80dc98f4ef3cb0b57a386812a9
Fourat ZOUARI <fourat@gmail.com>
2015-10-28 15:03:37 +0100

    Code rate threshold set to a higher level (9 instead of 8).

--- misc/pylint/run.sh
+++ misc/pylint/run.sh
@@ -3,7 +3,7 @@
 pylint --rcfile=./misc/pylint/pylintrc jasmin|tee /dev/stderr > /tmp/pylint-outcome
 RATE=$(grep "Your code has been rated" /tmp/pylint-outcome |awk '{print $7}'|grep -o '[0-9]\+.[0-9]\+')
 
-if [ $(echo "$RATE > 8" |bc) -eq 1 ]; then
+if [ $(echo "$RATE > 9" |bc) -eq 1 ]; then
   exit 0
 else
   exit 1
