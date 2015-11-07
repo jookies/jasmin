e469ab6b014fecf82ed08aaf5b92e1317ea89e93
Fourat ZOUARI <fourat@gmail.com>
2015-11-07 18:56:54 +0100

    Updated release version

--- jasmin/__init__.py
+++ jasmin/__init__.py
@@ -5,8 +5,8 @@
 
 MAJOR = 0
 MINOR = 8
-PATCH = 0
-META = 'dev'
+PATCH = 1
+META = 'b'
 
 def get_version():
     "Will return Jasmin's version"
@@ -14,7 +14,7 @@ def get_version():
 
 def get_release():
     "PEP 440 format"
-    return '%s.%s.%s%s' % (MAJOR, MINOR, META, PATCH)
+    return '%s.%s%s%s' % (MAJOR, MINOR, META, PATCH)
 
 __version__ = get_version()
 __release__ = get_release()
