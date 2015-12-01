712bbdab105b1553cc118c66760c7824a9b6f8ea
Fourat ZOUARI <fourat@gmail.com>
2015-12-01 03:38:39 +0100

    Updated sms_logger script:
    * To handle multipart messages
    * Save a binary copy of the message
    * Handle utf8 messages

--- misc/scripts/sms_logger.py
+++ misc/scripts/sms_logger.py
@@ -7,7 +7,9 @@ Requirement:
     `msgid`            VARCHAR(45) PRIMARY KEY,
     `source_addr`      VARCHAR(40),
     `destination_addr` VARCHAR(40) NOT NULL CHECK (`destination_addr` <> ''),
+    `pdu_count`        TINYINT(3) DEFAULT 1,
     `short_message`    BLOB,
+    `binary_message`   BLOB,
     `status`           VARCHAR(15) NOT NULL CHECK (`status` <> ''),
     `uid`              VARCHAR(15) NOT NULL CHECK (`uid` <> ''),
     `created_at`       DATETIME NOT NULL,
@@ -16,13 +18,14 @@ Requirement:
     INDEX `sms_log_3` (`created_at`),
     INDEX `sms_log_4` (`created_at`, `uid`),
     INDEX `sms_log_5` (`created_at`, `uid`, `status`)
-  );
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 TODO:
 - Add billing info
 """
 
 import pickle
+import binascii
 from twisted.internet.defer import inlineCallbacks
 from twisted.internet import reactor
 from twisted.internet.protocol import ClientCreator
@@ -57,9 +60,9 @@ def gotConnection(conn, username, password):
 
     #Connection parameters - Fill this info with your MySQL server connection parameters
     db = mdb.connect(
-        user='jasmin', 
-        passwd='jadmin', 
-        host='127.0.0.1', 
+        user='jasmin',
+        passwd='jadmin',
+        host='127.0.0.1',
         db='jasmin')
 
     print "Connected to MySQL"
@@ -82,25 +85,53 @@ def gotConnection(conn, username, password):
             if qmsg['source_addr'] is None:
                 qmsg['source_addr'] = ''
 
-            cursor.execute("""INSERT INTO submit_log (msgid, source_addr, destination_addr, short_message, status, uid, created_at) 
-                    VALUES (%s, %s, %s, %s, %s, %s, %s);
+            cursor.execute("""INSERT INTO submit_log (msgid, source_addr, pdu_count,
+                                                      destination_addr, short_message,
+                                                      status, uid, created_at, binary_message)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
                 """, (
                         props['message-id'],
                         qmsg['source_addr'],
+                        qmsg['pdu_count'],
                         qmsg['destination_addr'],
                         qmsg['short_message'],
                         pdu.status,
                         qmsg['bill'].user.uid,
                         props['headers']['created_at'],
+                        qmsg['binary_message'],
                     )
                 )
             db.commit()
         elif msg.routing_key[:10] == 'submit.sm.':
+            pdu_count = 1
+            short_message = pdu.params['short_message']
+
+            # Is it a multipart message ?
+            while hasattr(pdu, 'nextPdu'):
+                # Remove UDH from first part
+                if pdu_count == 1:
+                    short_message = short_message[6:]
+
+                pdu = pdu.nextPdu
+
+                # Update values:
+                pdu_count += 1
+                short_message += pdu.params['short_message'][6:]
+
+            # Save short_message bytes
+            binary_message = binascii.hexlify(short_message)
+
+            # If it's a binary message, assume it's utf_16_be encoded
+            if pdu.params['data_coding'] == 8:
+                short_message = short_message.decode('utf_16_be')
+
             q[props['message-id']] = {
                 'bill': pickle.loads(props['headers']['submit_sm_resp_bill']),
                 'destination_addr': pdu.params['destination_addr'],
                 'source_addr': pdu.params['source_addr'],
-                'short_message': pdu.params['short_message'],
+                'pdu_count': pdu_count,
+                'short_message': short_message.encode('utf_8'),
+                'binary_message': binary_message,
             }
         else:
             print 'unknown route'
@@ -127,9 +158,9 @@ if __name__ == "__main__":
     spec = txamqp.spec.load(spec_file)
 
     # Connect and authenticate
-    d = ClientCreator(reactor, 
-        AMQClient, 
-        delegate=TwistedDelegate(), 
+    d = ClientCreator(reactor,
+        AMQClient,
+        delegate=TwistedDelegate(),
         vhost=vhost,
         spec=spec).connectTCP(host, port)
     d.addCallback(gotConnection, username, password)
