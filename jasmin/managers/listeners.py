9ef15fe622bfb40a25165b35710cacc0637c69a9
zoufou <fourat@gmail.com>
2016-01-19 22:44:49 +0000

    Yielding calls to redisClient to block on redis db feedbacks

--- jasmin/managers/listeners.py
+++ jasmin/managers/listeners.py
@@ -438,9 +438,9 @@ class SMPPClientSMListener(object):
                                        r.response.params['message_id'], msgid, dlr_expiry)
                         hashKey = "queue-msgid:%s" % r.response.params['message_id'].upper().lstrip('0')
                         hashValues = {'msgid': msgid, 'connector_type': 'httpapi',}
-                        self.redisClient.setex(hashKey,
-                                               dlr_expiry,
-                                               pickle.dumps(hashValues, self.pickleProtocol))
+                        yield self.redisClient.setex(hashKey,
+                                                     dlr_expiry,
+                                                     pickle.dumps(hashValues, self.pickleProtocol))
                 elif dlr is not None and dlr['sc'] == 'smppsapi':
                     self.log.debug('There is a SMPPs mapping for msgid[%s] ...', msgid)
 
@@ -484,9 +484,9 @@ class SMPPClientSMListener(object):
                                            r.response.params['message_id'], msgid, smpps_map_expiry)
                             hashKey = "queue-msgid:%s" % r.response.params['message_id'].upper().lstrip('0')
                             hashValues = {'msgid': msgid, 'connector_type': 'smppsapi',}
-                            self.redisClient.setex(hashKey,
-                                                   smpps_map_expiry,
-                                                   pickle.dumps(hashValues, self.pickleProtocol))
+                            yield self.redisClient.setex(hashKey,
+                                                         smpps_map_expiry,
+                                                         pickle.dumps(hashValues, self.pickleProtocol))
             else:
                 self.log.warn('No valid RC were found while checking msg[%s] !', msgid)
 
@@ -754,7 +754,7 @@ class SMPPClientSMListener(object):
                                   'total_segments':total_segments,
                                   'msg_ref_num':msg_ref_num,
                                   'segment_seqnum':segment_seqnum}
-                    self.redisClient.hset(
+                    yield self.redisClient.hset(
                         hashKey, segment_seqnum, pickle.dumps(hashValues, self.pickleProtocol)).addCallback(
                             self.concatDeliverSMs,
                             splitMethod,
