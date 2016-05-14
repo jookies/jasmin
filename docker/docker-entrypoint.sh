5b8e6e9a75e9e09a9b19fd6fe5e88d0c8705ef3f
Nick <kutuzovnick@gmail.com>
2016-05-14 03:54:59 +0300

    Docker conf update

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 set -e
-
-/etc/init.d/rabbitmq-server start
-/etc/init.d/redis-server start
-
+rm -rf /tmp/*
 if [ "$2" = "--enable-interceptor-client" ]; then
   echo 'Starting interceptord'
   interceptord.py &
 fi
-
 echo 'Starting jasmind'
 exec "$@"
+
