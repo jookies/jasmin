a32f418736d5822560a1777bdebad7ceb6ba56b9
zoufou <fourat@gmail.com>
2016-09-15 00:12:51 +0100

    docs: removed unused sphinx extensions.

--- misc/doc/sources/conf.py
+++ misc/doc/sources/conf.py
@@ -14,7 +14,7 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.imgmath', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode']
+extensions = []
 todo_include_todos=True
 
 # Add any paths that contain templates here, relative to this directory.
