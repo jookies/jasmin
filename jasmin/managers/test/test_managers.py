8b0e01ba46848df704e200bfa70c6d24a9dbe5b0
The Z <support@jasminsms.com>
2017-12-03 07:14:42 +0100

    Fixing failed test

--- jasmin/managers/test/test_managers.py
+++ jasmin/managers/test/test_managers.py
@@ -566,14 +566,10 @@ class ClientConnectorTestCases(SMPPClientPBProxyTestCase):
         localConfig = copy.copy(self.defaultConfig)
         localConfig.reconnectOnConnectionFailure = False
         yield self.add(localConfig)
-        yield waitFor(0.2)
         yield self.start(localConfig.id)
-        yield waitFor(0.2)
         startRet = yield self.start(localConfig.id)
-        yield waitFor(0.2)
 
-        print startRet
-        self.assertEqual(False, startRet)
+        self.assertEqual(True, startRet)
 
         yield self.stopall()
 
