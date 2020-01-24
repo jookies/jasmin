64e1ff2bcc9ac6516d07956dd319a0f735c291e1
The Z <support@jasminsms.com>
2020-01-24 23:00:56 +0100

    Fixed test case jasmin.protocols.smpp.test.test_smpp_client.LongSubmitSmGenericNackTestCasejasmin.protocols.smpp.test.test_smpp_client

--- jasmin/protocols/smpp/test/test_smpp_client.py
+++ jasmin/protocols/smpp/test/test_smpp_client.py
@@ -1348,7 +1348,6 @@ class LongSubmitSmGenericNackTestCase(SimulatorTestCase):
             destination_addr=self.destination_addr,
             short_message=self.concatenated2Msgs,
         )
-        yield smpp.sendDataRequest(SubmitSmPDU)
         yield self.assertFailure(smpp.sendDataRequest(SubmitSmPDU), SMPPClientConnectionCorruptedError)
 
         # Unbind & Disconnect
