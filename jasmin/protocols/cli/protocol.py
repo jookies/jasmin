39cdda0abdb99535baa6ddcffb2072218e6debe6
Fourat ZOUARI <fourat@gmail.com>
2020-10-02 19:23:10 +0100

    Fixed TAB handling in jcli.

--- jasmin/protocols/cli/protocol.py
+++ jasmin/protocols/cli/protocol.py
@@ -127,7 +127,8 @@ class CmdProtocol(recvline.HistoricRecvLine):
 
         # Binary to str
         # Py2 > Py3 migration (Refs #171)
-        line = line.decode('ascii')
+        if isinstance(line, bytes):
+            line = line.decode('ascii')
 
         i, n = 0, len(line)
         while i < n and line[i] in self.identchars:
@@ -181,8 +182,24 @@ class CmdProtocol(recvline.HistoricRecvLine):
 
         return foundCommands
 
+    def handle_RETURN(self):
+        # Str to binary
+        # Py2 > Py3 migration (Refs #171)
+        for _ in range(len(self.lineBuffer)):
+            if not isinstance(self.lineBuffer[_], bytes):
+                self.lineBuffer[_] = self.lineBuffer[_].encode('ascii')
+
+        return recvline.HistoricRecvLine.handle_RETURN(self)
+
     def handle_TAB(self):
-        line = ''.join(self.lineBuffer)
+        line = ''
+        for lb in self.lineBuffer:
+            # Binary to str
+            # Py2 > Py3 migration (Refs #171)
+            if isinstance(lb, bytes):
+                line += lb.decode('ascii')
+            else:
+                line += lb
         self.log.debug('[sref:%s] Tabulation: %s', self.sessionRef, line)
 
         cmd, arg, line = self.parseline(line)
