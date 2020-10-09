fb6e111c2232e48694fc02fe6a7deec448e9910c
The Z <support@jasminsms.com>
2020-10-09 20:57:23 +0100

    Manually commiting PR #899

--- jasmin/queues/factory.py
+++ jasmin/queues/factory.py
@@ -1,6 +1,8 @@
 # pylint: disable=E0203
 import sys
 import logging
+import os
+import signal
 from logging.handlers import TimedRotatingFileHandler
 from twisted.internet.protocol import ClientFactory
 from twisted.internet import defer, reactor
@@ -95,6 +97,7 @@ class AmqpFactory(ClientFactory):
             self.connectDeferred.errback(reason)
             self.exitDeferred.callback(self)
             self.log.info("Exiting.")
+            os.kill(os.getpid(), signal.SIGINT)
 
     def clientConnectionLost(self, connector, reason):
         """Connection lost
@@ -115,6 +118,7 @@ class AmqpFactory(ClientFactory):
         else:
             self.exitDeferred.callback(self)
             self.log.info("Exiting.")
+            os.kill(os.getpid(), signal.SIGINT)
 
     def reConnect(self, connector=None):
         if connector is None:
