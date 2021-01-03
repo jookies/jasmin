9f09786ad1fbd750b43b10f7a94278f32877405c
The Z <support@jasminsms.com>
2021-01-03 21:58:21 +0100

    Fixing nfpm packaging

--- misc/packaging/scripts/common-preremove.sh
+++ misc/packaging/scripts/common-preremove.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 set -e
 
-if [ "$1" = remove ]; then
-  # Stop and disable jasmind service
-  /bin/systemctl stop jasmind
-  /bin/systemctl disable jasmind
-  /bin/systemctl daemon-reload
+PYPI_NAME="jasmin"
+
+pip3 uninstall -y "$PYPI_NAME"
+
+# Stop and disable jasmind service
+/bin/systemctl stop jasmind
+/bin/systemctl disable jasmind
+/bin/systemctl daemon-reload
+
+# python3-falcon package is not available in centos/rhel 8
+# this is a workaround
+if [ "$(grep -Ei 'centos|rhel|fedora' /etc/*release)" ]; then
+  pip3 uninstall -y falcon==2.0.0
 fi
