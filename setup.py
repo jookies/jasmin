52962a520241d33d9fc5c196570899c0de24c2e3
farirat <1020111+farirat@users.noreply.github.com>
2023-11-15 15:05:55 +0100

    version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def parse_requirements(filename):
 
 setup(
     name="jasmin",
-    version='0.11.0',
+    version='0.11.1',
     author="Jookies LTD",
     author_email="jasmin@jookies.net",
     url="https://www.jasminsms.com",
